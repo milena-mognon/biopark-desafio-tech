709441068d3583bf5f50df0817736ad3c35f15dd
milena-mognon <milenamognon@gmail.com>
2020-11-13 18:04:51 -0300

    fix: added lib and fixed error with AppError

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
 import routes from '@shared/infra/http/routes';
